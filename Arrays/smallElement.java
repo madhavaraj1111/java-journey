000aa3d68d78d960fc9b3e5881ef170dca04145e
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-07-08 09:27:12 +0530

    Combined the smallElement code in one file

--- Arrays/smallElement.java
+++ Arrays/smallElement.java
@@ -21,3 +21,25 @@ class Codechef
 	    System.out.println("The minimum value in the array is "+min);
 	}
 }
+
+// Built in Methods Approach
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Integer [] arr={-7,1,-3,-10};
+	    getMin(arr);
+ 
+	}
+	
+	static void getMin(Integer[] arr){
+	    List<Integer> list=Arrays.asList(arr);
+	    int min=Collections.min(list);
+	    System.out.println(min);
+	}
+}
