44c5e6c2dd63fdd0a1a65680bd9eb2df06ea8ea1
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-07-08 11:04:36 +0530

    Added the second smallest Element code

--- Arrays/secondSmallestElement.java
+++ Arrays/secondSmallestElement.java
@@ -1,3 +1,53 @@
+// One For loop with conditions
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[] numArray={2,3,1,4,5};
+		findSecondSmallest(numArray);
+ 
+	}
+	
+	static void findSecondSmallest(int[] arr){
+	    if(arr.length<2){
+	        System.out.println("Only one Array Element found");
+	        return;
+	    }
+	    
+	    int smallest = Integer.MAX_VALUE;
+	    int secSmallest=Integer.MAX_VALUE;
+	    
+	    for(int i=0;i<arr.length;i++){
+	        if(arr[i]<smallest&&arr[i]!=smallest){
+	            secSmallest=smallest;
+	            smallest=arr[i];
+	        }
+	        
+	        else if(arr[i]>smallest&&arr[i]<secSmallest){
+	            secSmallest=arr[i];
+	        }
+	        
+	    }
+	   // Edge case [10,10] so then second small would remain MAX integer so here by the condition.
+	   
+	   if(secSmallest==Integer.MAX_VALUE){
+	       System.out.println("There is no second smallest element");
+	   }
+	   else{
+	       System.out.println(secSmallest);
+	   }
+	    
+	}
+}
+
+
+// Two loops 
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
