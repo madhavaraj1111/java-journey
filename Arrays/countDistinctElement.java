887d379f0c488dbdde3976cdcdeeb85d5b3351ad
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-07-17 10:13:08 +0530

    Distinct element Delay Method code Added

--- Arrays/countDistinctElement.java
+++ Arrays/countDistinctElement.java
@@ -28,3 +28,32 @@ class Codechef
 	    System.out.println(Arrays.toString(visited));
 	}
 }
+
+
+// lazy-delay -->Until we find the last possible element that will be distinct one
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int[]arr={5,8,5,8,7,10,5};
+		findDisElemCount(arr);
+	}
+	static void findDisElemCount(int[]arr){
+	    int count=0;
+	    for(int i=0;i<arr.length;i++){
+	        int flag=0;
+	        for(int j=i+1;j<arr.length;j++){
+	            if(arr[i]==arr[j]){
+	                flag=1;
+	            }
+	        }
+	        
+	        if(flag==0){
+	            count++;
+	        }
+	    }
+	    System.out.println(count);
+	}
+	
+}
