4f5011562f2a1347982dd147947a28e5bda532ec
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-07-20 14:29:45 +0530

    frequency of elements lazy delay + sorted

--- Arrays/frequencyOfElem.java
+++ Arrays/frequencyOfElem.java
@@ -24,3 +24,27 @@ public class frequencyOfElem{
 	}
 
 }
+
+
+//Lazy delay + Sorted , frequency of elements in an array
+// Time Complexity: O(n log n) due to sorting
+import java.util.Arrays;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+      int[]arr={2,3,5,3,2,3,2,4};
+      frequency(arr);
+	}
+	static void frequency(int[]arr){
+	         Arrays.sort(arr);
+	     for(int i=0;i<arr.length;i++){
+	         int count=1;
+	         while(i<arr.length-1&&(arr[i]==arr[i+1])){
+	             count++;
+	             i++;
+	         }
+	         System.out.println(arr[i]+":"+count);
+	     }
+	}
+}
