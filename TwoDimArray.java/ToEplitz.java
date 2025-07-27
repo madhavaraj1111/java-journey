efb3b068b5052bc2426188dee1f02bdaacbc5023
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-07-27 22:33:35 +0530

    Clean code Toeplitz

--- TwoDimArray.java/ToEplitz.java
+++ TwoDimArray.java/ToEplitz.java
@@ -30,3 +30,18 @@ public class ToEplitz {
 
 // The code checks if a given 2D matrix is a Toeplitz matrix, where each descending diagonal from left to right has the same elements.
 
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+      
+    }
+}
