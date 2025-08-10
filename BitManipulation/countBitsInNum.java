6651be6cc3471a729c674aab25cd905d0ba98642
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-08-10 13:29:24 +0530

    Counting bits in number

---
+++ BitManipulation/countBitsInNum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     int n = 5;
+	     int mask =1;
+	     int count=0;
+	     while(n!=0){
+	         if((n&mask)==1){
+	             count++;
+	         }
+	         n=n>>1;
+	     }
+	     System.out.println(count);
+           
+       
+	}
+}
