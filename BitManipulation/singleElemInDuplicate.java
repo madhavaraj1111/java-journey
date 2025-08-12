bcbe8ec850a4f5641fd095dc86ab562946205303
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-08-13 00:20:54 +0530

    Using HashMap to find unique elem

--- BitManipulation/singleElemInDuplicate.java
+++ BitManipulation/singleElemInDuplicate.java
@@ -11,3 +11,41 @@ class Codechef
 
 	}
 }
+
+// HashMap
+
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        int[] arr = {
+            1,
+            1,
+            2,
+            3,
+            3,
+            4,
+            4,
+            8,
+            8
+        };
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (int key: map.keySet()) {
+
+
+            if (map.get(key) == 1) {
+                System.out.println(key + ":" + map.get(key));
+            }
+        }
+
+    }
+}
