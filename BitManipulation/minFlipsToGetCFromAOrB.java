3c0734db45623e1229442dcce1a3e3242451a82b
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-08-16 16:26:01 +0530

    Updated version of minFlips

--- BitManipulation/minFlipsToGetCFromAOrB.java
+++ BitManipulation/minFlipsToGetCFromAOrB.java
@@ -32,3 +32,31 @@ class Codechef {
 
     }
 }
+// Updated version
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        int a = 8;
+        int b = 3;
+        int c = 5;
+        int flip=0;
+        while (a != 0 || b != 0 || c != 0) {
+            int X1=a&1;
+            int X2=b&1;
+            int X3=c&1;
+            
+            if((X1|X2)!=X3){
+                if((X1&X2)==1){ flip+=2;}
+                else {flip+=1;}
+            }
+            
+         
+            a=a>>1;
+            b=b>>1;
+            c=c>>1;
+        }
+        System.out.println(flip);
+
+    }
+}
