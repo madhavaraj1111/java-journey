25dba669b1f1bd8b00a210f6054bd847ddb38b95
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-08-16 16:54:21 +0530

    Factorial java in Recursion

---
+++ factorial.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+    public static void main(String[] args)
+    {
+       System.out.println(factorial(5));        
+
+    }
+    
+    static int factorial(int n){
+        if(n>1){
+            return n*factorial(n-1);
+        }
+        else{
+            return 1;
+        }
+    }
+}
