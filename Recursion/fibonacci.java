e8efaef93b85144df1eb98bc554ab0a39795c837
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-08-21 11:06:07 +0530

    Tribonacci series

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -19,3 +19,36 @@ class Codechef
 	    }
 	}
 }
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        System.out.println(fib(7));
+
+    }
+
+    static int fib(int n) {
+
+        if (n <= 2) {
+
+            if (n == 2) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+
+        }
+
+        else {
+
+            return fib(n - 1) + fib(n - 2) + fib(n - 3);
+        }
+    }
+}
