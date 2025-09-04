53ae8541bc76f7305dde6b09c95d421c87d2b641
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-09-04 21:21:59 +0530

    Optimal approach for pizzeria

--- Divide&Conquer/pizzeria.java
+++ Divide&Conquer/pizzeria.java
@@ -28,3 +28,54 @@ class Solution {
         remainingMoney(amount, pocketMoney);
     }
 }
+
+// Optimal
+import java.util.Arrays;
+class Codechef
+{
+    public static void main(String[] args) {
+        int[] amount = {
+           10, 2, 6, 4, 1
+        };
+        int[] pocketMoney = {
+           19, 5
+        };
+
+        int sum = 0;
+        for (int i = 0; i < amount.length; i++) {
+            sum = amount[i] + sum;
+            amount[i] = sum;
+        }
+
+        for (int j = 0; j < pocketMoney.length; j++) {
+            amountRem(amount, pocketMoney[j]);
+        }
+    }
+
+
+    static void amountRem(int[] amount, int pocketMoney) {
+        int low = 0;
+        int high = amount.length - 1;
+        int mid = 0;
+        int remAmount = pocketMoney;
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if ((pocketMoney - amount[mid]) >= 0) {
+                remAmount = pocketMoney - amount[mid];
+                low = mid + 1;
+            }
+            
+            if((pocketMoney - amount[mid]) < 0){
+                high=mid-1;
+            }
+            
+            
+        }
+
+
+        System.out.println(remAmount);
+    }
+
+
+}
