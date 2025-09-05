6be36075fecfdfba29cca95b3e9475e82801284e
madhavaraj1111 <madhavaraj1111@gmail.com>
2025-09-05 18:26:43 +0530

    Kadanes

--- Divide&Conquer/maxSubSum.java
+++ Divide&Conquer/maxSubSum.java
@@ -30,3 +30,30 @@ class Solution {
     }
 }
 
+// Kadanes optimal
+
+class Solution {
+    static int maxSubArray(int[] nums) {
+        int currentSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+             int sum=nums[i]+currentSum;
+             if(sum>=nums[i]){
+                 currentSum=sum;
+             }
+             else if(sum < nums[i]){
+                 currentSum=nums[i];
+             }
+             
+             if(currentSum>maxSum){
+                 maxSum=currentSum;
+             }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+}
